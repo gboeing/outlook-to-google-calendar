00a7ab0bf93d347d1c874cb799c3fd00543adf9b
Geoff Boeing <boeing@usc.edu>
2020-07-25 10:09:04 -0700

    add sample config

---
+++ config_sample.py
@@ -0,0 +1,17 @@
+# outlook
+outlook_client_id = "your-client-id-here"
+outlook_client_secret = "your-client-secret-here"
+outlook_scopes = ["basic", "calendar"]
+outlook_token_path = "./credentials/"
+outlook_token_filename = "outlook_token.txt"
+previous_days = 40  # retrieve this many past days of events
+future_days = 365  # retrieve this many future days of events
+
+# google
+google_token_path = "./credentials/google_token.pickle"
+google_calendar_id = "your-calendar-id-here@group.calendar.google.com"
+
+# misc
+events_ts_json_path = "./events_ts.json"
+pause = 0.1
+force = False  # force full run even if no changes
