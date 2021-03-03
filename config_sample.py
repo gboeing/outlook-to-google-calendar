ae096584ecee825337f5368fcc5a77ccd0114389
LenAnderson <anderson.len@outlook.com>
2021-03-03 07:52:42 +0100

    update config sample for multiple calendars

--- config_sample.py
+++ config_sample.py
@@ -9,9 +9,14 @@ future_days = 365  # retrieve this many future days of events
 
 # google
 google_token_path = "./credentials/google_token.pickle"
-google_calendar_id = "your-calendar-id-here@group.calendar.google.com"
+
+# calendars
+calendars = [
+	["You Outlook Calendar Name Here", "your-calendar-id-here@group.calendar.google.com"]
+]
+
 
 # misc
-events_ts_json_path = "./events_ts.json"
+events_ts_json_path = "./events_ts_{0}.json"
 pause = 0.1
 force = False  # force full run even if no changes
